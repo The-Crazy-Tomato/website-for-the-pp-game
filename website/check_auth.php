9ac920046e1e1c625cb3f5e339d3541734f8ec97
The-Crazy-Tomato <zenatueva13385@gmail.com>
2026-03-09 22:38:15 +0700

    Delete website directory

--- website/check_auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Если пользователь не вошел в систему
-if (!isset($_SESSION['user_id'])) {
-    header("Location: ../auth/index.html"); // Перенаправляем на страницу входа
-    exit();
-}
-?>
